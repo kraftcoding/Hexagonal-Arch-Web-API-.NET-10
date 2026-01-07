c2656dd185654e3203a44db4537ed68f597ad866
kraftcoding <i.elg.alzaa@gmail.com>
2026-01-07 02:50:22 -0800

    add ignored files

---
+++ src/Application/Ports/Out/EventsDispatching/IEventsDispatcher.cs
@@ -0,0 +1,9 @@
+﻿using NetCoreHexagonal.Domain.Commons;
+
+namespace NetCoreHexagonal.Application.Ports.Out.EventsDispatching
+{
+    public interface IEventsDispatcher
+    {
+        Task DispatchAsync(IEvent @event);
+    }
+}
