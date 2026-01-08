7520d488d01c0cb6a30948312004f083918c6957
kraftcoding <i.elg.alzaa@gmail.com>
2026-01-08 04:08:16 -0800

    migration added

---
+++ src/Adapters/Out/Persistence/Design/Migrations/20260108120737_Update.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetCoreHexagonal.Persistence.Design.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "BookId",
+                table: "Enrollment",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollment_BookId",
+                table: "Enrollment",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollment_Books_BookId",
+                table: "Enrollment",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollment_Books_BookId",
+                table: "Enrollment");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Enrollment_BookId",
+                table: "Enrollment");
+
+            migrationBuilder.DropColumn(
+                name: "BookId",
+                table: "Enrollment");
+        }
+    }
+}
